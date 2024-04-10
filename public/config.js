145e4b4e39ca4ba8f1bcb5c194ab0f23d760ff82
dilantha <dilantha.tyrant@gmail.com>
2024-04-11 01:09:23 +0200

    adding config again

--- public/config.js
+++ public/config.js
@@ -1,9 +1,9 @@
 window.configs = {
     apiUrl: '/choreo-apis/nion/newsapi/newsapi-131/v1.0',
     asgardeoConfig: {
-        "signInRedirectURL": "https://a69661f5-a9fc-4c94-8a4f-230320c947a6.e1-us-east-azure.choreoapps.dev",
-          "signOutRedirectURL": "https://a69661f5-a9fc-4c94-8a4f-230320c947a6.e1-us-east-azure.choreoapps.dev",
-          "clientID": "XcadjmN5Og7DlelTkZpxJA7saoYa",
+        "signInRedirectURL": "https://e80194f5-7bfc-40ec-8537-fbe3717cae1f.e1-us-east-azure.choreoapps.dev",
+          "signOutRedirectURL": "https://e80194f5-7bfc-40ec-8537-fbe3717cae1f.e1-us-east-azure.choreoapps.dev",
+          "clientID": "xu1YISffRwpp0L1J0coXBNUDWtsa",
           "baseUrl": "https://api.asgardeo.io/t/dilantha",
           "scope": [ "openid","profile" ]
     }
